22972bb8fe9e2d5aacfd59b8b8554274534def9f
Ântones Lima <antoneslima@gmail.com>
2023-07-11 02:09:10 -0300

    add audios; add trezena.html e hinosepontos.html; changes index.html

--- main.js
+++ main.js
@@ -17,6 +17,13 @@ function exibirMusicas(musicas) {
         tituloTom.textContent = `${musica.id}. ${musica.Titulo} - Tom: ${musica.Tom}`;
         divMusica.appendChild(tituloTom);
 
+        const testaudio = document.createElement('audio');
+        testaudio.src = `./audios/hinosepontos/${musica.id}.mp3`
+        testaudio.controls = true;
+        testaudio.autoplay = false;
+        testaudio.loop = true;
+        testaudio.preload = "auto"; 
+
         // const recebidoPor = document.createElement('div');
         // recebidoPor.classList.add('recebido-por');
         // recebidoPor.textContent = 'Recebido por: ' + musica['Recebido por'];
@@ -26,6 +33,7 @@ function exibirMusicas(musicas) {
         letra.classList.add('letra');
         letra.textContent = musica.Letra;
         divMusica.appendChild(letra);
+        divMusica.appendChild(testaudio);
 
         musicasContainer.appendChild(divMusica);
     });
---
+++ trezena.js
@@ -0,0 +1,43 @@
+function carregarMusicas() {
+    fetch('./Json/trezena_StAntonio_2023.json')
+        .then(response => response.json())
+        .then(data => exibirMusicas(data))
+        .catch(error => console.error('Ocorreu um erro ao carregar as músicas:', error));
+}
+
+function exibirMusicas(musicas) {
+    const musicasContainer = document.getElementById('musicas');
+
+    musicas.forEach(musica => {
+        const divMusica = document.createElement('div');
+        divMusica.classList.add('musica');
+
+        const tituloTom = document.createElement('div');
+        tituloTom.classList.add('titulo-tom');
+        tituloTom.textContent = `${musica.id}. ${musica.Titulo} - Tom: ${musica.Tom}`;
+        divMusica.appendChild(tituloTom);
+
+        const testaudio = document.createElement('audio');
+        testaudio.src = `./audios/trezena/${musica.id}.mp3`
+        testaudio.controls = true;
+        testaudio.autoplay = false;
+        testaudio.loop = true;
+        testaudio.preload = "auto";
+        testaudio.textContent = "This is the text inside the paragraph.";
+
+        // const recebidoPor = document.createElement('div');
+        // recebidoPor.classList.add('recebido-por');
+        // recebidoPor.textContent = 'Recebido por: ' + musica['Recebido por'];
+        // divMusica.appendChild(recebidoPor);
+
+        const letra = document.createElement('div');
+        letra.classList.add('letra');
+        letra.textContent = musica.Letra;
+        divMusica.appendChild(letra);
+        divMusica.appendChild(testaudio);
+
+        musicasContainer.appendChild(divMusica);
+    });
+}
+
+carregarMusicas();
